29d92a272f9cf787186ce95e12f023c409ae356a
n22550946 <n22550946@hotmail.com>
2018-07-09 21:40:59 +0800

    1.增加ViewModel 2.增加記帳類別Enum

---
+++ MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/ViewModels/DailyAccountViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MVCHomeWorkMoneyTemplate.EnumType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHomeWorkMoneyTemplate.ViewModels
+{
+    public class DailyAccountViewModel
+    {
+        public int AccountType { get; set; }
+
+        public DateTime AccountingDate { get; set; }
+
+        public int AccountingAmount { get; set; }
+    }
+}
