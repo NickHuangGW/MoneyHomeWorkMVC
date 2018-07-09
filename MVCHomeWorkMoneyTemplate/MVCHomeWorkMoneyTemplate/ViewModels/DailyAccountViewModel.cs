7ee6845cfd99af9a618e1035bd764ceaf1636630
n22550946 <n22550946@hotmail.com>
2018-07-09 22:32:38 +0800

    新增DisplayName

--- MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/ViewModels/DailyAccountViewModel.cs
+++ MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/ViewModels/DailyAccountViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MVCHomeWorkMoneyTemplate.EnumType;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,13 @@ namespace MVCHomeWorkMoneyTemplate.ViewModels
 {
     public class DailyAccountViewModel
     {
+        [Display(Name ="類別")]
         public int AccountType { get; set; }
-
+        [Display(Name = "日期")]
         public DateTime AccountingDate { get; set; }
-
+        [Display(Name = "金額")]
         public int AccountingAmount { get; set; }
-
+        [Display(Name = "備註")]
         public string Description { get; set; }
     }
 }
