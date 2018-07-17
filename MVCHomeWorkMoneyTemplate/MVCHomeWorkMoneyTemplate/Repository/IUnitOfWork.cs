54ce0f6c613684098396858fffc3bf4d4c344de6
n22550946 <n22550946@hotmail.com>
2018-07-18 03:04:29 +0800

    實作UnitOfWork

---
+++ MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/Repository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCHomeWorkMoneyTemplate.Repository
+{
+    public interface IUnitOfWork
+    {
+        DbContext UnitDbContext { get; set; }
+
+        void CommitTrans();
+    }
+}
---
+++ MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/Repository/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCHomeWorkMoneyTemplate.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext UnitDbContext { get; set; }
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            UnitDbContext = dbContext;
+        }
+
+        public void CommitTrans()
+        {
+            UnitDbContext.SaveChanges();
+        }
+    }
+}
