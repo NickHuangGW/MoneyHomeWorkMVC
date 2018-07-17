a32aef3a0b97823b5bd8bb33f2dd676b51bf8a35
n22550946 <n22550946@hotmail.com>
2018-07-18 03:35:58 +0800

    新增GenericRepostiory

---
+++ MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/Repository/GenericRepostiory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace MVCHomeWorkMoneyTemplate.Repository
+{
+    public class GenericRepostiory<T> : IGenericRepostiory<T> where T : class
+    {
+        public IUnitOfWork UnitOfWork { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Create(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Query(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/Repository/IGenericRepostiory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCHomeWorkMoneyTemplate.Repository
+{
+    public interface IGenericRepostiory<T>
+    {
+        IUnitOfWork UnitOfWork { get; set; }
+
+        IQueryable<T> GetAll();
+
+        IQueryable<T> Query(Expression<Func<T, bool>> predicate);
+
+        void Create(T model);
+
+        void Delete(T model);
+
+        void Update(T model);
+
+    }
+}
