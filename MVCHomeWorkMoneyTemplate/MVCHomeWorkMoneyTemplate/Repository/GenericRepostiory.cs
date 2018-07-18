1518fdf845292049af144220ed2fd99458110713
n22550946 <n22550946@hotmail.com>
2018-07-18 10:17:41 +0800

    實作UnitOfWork

--- MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/Repository/GenericRepostiory.cs
+++ MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/Repository/GenericRepostiory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web;
@@ -8,30 +9,39 @@ namespace MVCHomeWorkMoneyTemplate.Repository
 {
     public class GenericRepostiory<T> : IGenericRepostiory<T> where T : class
     {
-        public IUnitOfWork UnitOfWork { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IUnitOfWork UnitOfWork { get; set; }
+
+        private DbSet<T> EfDbSet { get; set; }
+
+        public GenericRepostiory(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+            EfDbSet = UnitOfWork.UnitDbContext.Set<T>();
+        }
 
         public void Create(T model)
         {
-            throw new NotImplementedException();
+            EfDbSet.Add(model);
         }
 
         public void Delete(T model)
         {
-            throw new NotImplementedException();
+            EfDbSet.Remove(model);
         }
 
         public IQueryable<T> GetAll()
         {
-            throw new NotImplementedException();
+            return EfDbSet.AsQueryable();
         }
 
         public IQueryable<T> Query(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return EfDbSet.Where(predicate);
         }
 
         public void Update(T model)
         {
+            //未實作
             throw new NotImplementedException();
         }
     }
