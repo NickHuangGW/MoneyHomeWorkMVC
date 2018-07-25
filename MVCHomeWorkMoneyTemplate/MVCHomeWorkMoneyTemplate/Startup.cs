797977a6db732f72377bc75b2eb2c44ad6f6b251
n22550946 <n22550946@hotmail.com>
2018-07-26 02:49:37 +0800

    加入驗證

---
+++ MVCHomeWorkMoneyTemplate/MVCHomeWorkMoneyTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHomeWorkMoneyTemplate.Startup))]
+namespace MVCHomeWorkMoneyTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
